227cb94b043375b41904e961e94ac052bcc60d60
임재진(메가존) <mzjjlim@lgupluspartners.co.kr>
2023-10-17 00:28:07 +0900

    스프링 시큐리티 자동 구성

---
+++ src/main/java/jocture/securitybasic/controller/HelloController.java
@@ -0,0 +1,18 @@
+package jocture.securitybasic.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info(">>> Hello!!");
+        return "Ok!";
+    }
+}
